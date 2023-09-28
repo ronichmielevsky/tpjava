d47905e1ca2582a453d48b252a0fa891a8ac8c7e
roni <47867214@ort>
2023-09-28 15:57:32 -0300

    falta poder registrarse con usuario y contraseña, no sabemos como pasarlo por parametro

--- Controllers/AcountController.cs
+++ Controllers/AcountController.cs
@@ -5,13 +5,38 @@
     namespace tpjava.Controllers;
     
 
+
     public class AcountController : Controller
+
+
 {
+    public IActionResult Lol()
+    {
+        return View("Acount");
+    }
+
+    public IActionResult Sas()
+    {
+        return View("IniciarSes");
+    }
 
      [HttpPost]
      public IActionResult AgregarAcount(string nombre, string username, string contraseña, string email, int telefono){
         Usuario user = new Usuario( nombre, username, contraseña, email, telefono);
-        return View();
+        BD.AgregarCuenta(user);
+        ViewBag.nombre=nombre;
+        ViewBag.username=username;
+        ViewBag.contraseña=contraseña;
+        ViewBag.email=email;
+        ViewBag.telefono=telefono;
+        return View("MostrarDatos");
     }
 
+ [HttpPost]
+     public IActionResult Registrarse(string username, string contraseña,string nombre, string email, int telefono){
+        ViewBag.roni=BD.VerificarCuenta(username, contraseña);
+        return View("ss");
+     }
+   
+     
 }
--- Models/BD.cs
+++ Models/BD.cs
@@ -1,14 +1,25 @@
 using System.Data.SqlClient;
 using System.Collections.Generic;
 using Dapper;
+
 static class BD{
     private static string _connectionString = @"server=localhost;DataBase=login;Trusted_connection=True;";
     
-public static void AgregarUsuario(Usuario user){
-    string SQL = "INSERT INTO Usuario(Nombre,UserName,Contraseña,Email,Telefono) VALUES(@nombre, @username, @contraseña, @email, @telefono)";
+public static void AgregarCuenta(Usuario user){
+    string SQL = "INSERT INTO Usuario(Nombre,UserName,Contraseña,Email,Telefono) VALUES (@nombre, @username, @contraseña, @email, @telefono)";
      using (SqlConnection db = new SqlConnection(_connectionString))
         {
-            db.Execute(SQL, new { pIdCandidato = can.IdCandidato, pIdPartido = can.idPartido, pApellido = can.Apellido, pNombre = can.Nombre, pFechaNacimiento = can.FechaNacimiento, pFoto = can.Foto, pPostulacion = can.Postulacion });
+            db.Execute(SQL, new { nombre = user.Nombre, username = user.UserName, contraseña = user.Contraseña, email = user.Email, telefono = user.Telefono});
         }
+}
+
+public static Usuario VerificarCuenta(string username, string contraseña){
+    Usuario roni = null;
+     using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string SQL = "SELECT * FROM Usuario WHERE UserName=@pusername AND Contraseña=@pcontraseña";
+            roni = db.QueryFirstOrDefault<Usuario>(SQL, new { pusername = username, pcontraseña = contraseña});
+        }
+        return roni;
 }
 }
